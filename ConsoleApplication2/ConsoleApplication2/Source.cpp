bc78fcd1b68c2e6a29fe3d9f9e43f546ad1feb1c
thatmich <47138011+thatmich@users.noreply.github.com>
2020-05-22 03:42:29 +0900

    changes before Advanced Procrastination (APs)

--- ConsoleApplication2/ConsoleApplication2/Source.cpp
+++ ConsoleApplication2/ConsoleApplication2/Source.cpp
@@ -9,13 +9,13 @@ using namespace std;
 
 int const max_value = 255;
 int const max_BINARY_value = 255;
-float const bK = 0.5;
-float const bR = 100;
+float const bK = 0.6;
+float const bR = 90;
 
 const double PI = 3.141592653589793238460;
 
-//Mat image = imread("C:\\Users\\wwwsu\\Source\\Repos\\MichiOCR\\ConsoleApplication2\\IMG_20200311_105406.jpg", IMREAD_GRAYSCALE);
-Mat image = imread("C:\\Users\\wwwsu\\Source\\Repos\\MichiOCR\\ConsoleApplication2\\IMG_20200210_102347.jpg", IMREAD_GRAYSCALE);
+Mat image = imread("C:\\Users\\wwwsu\\Source\\Repos\\MichiOCR\\ConsoleApplication2\\timesA.png", IMREAD_GRAYSCALE);
+//Mat image = imread("C:\\Users\\wwwsu\\Source\\Repos\\MichiOCR\\ConsoleApplication2\\IMG_20200210_102347.jpg", IMREAD_GRAYSCALE);
 Mat dst = image;
 /// Function Headers
 Mat binarization(Mat img);
@@ -23,7 +23,19 @@ float avgColor(Mat img);
 float sDeviation(Mat img);
 Mat straightening(Mat s_img);
 Mat rotate(Mat src, float angle);
-Mat skeleton(Mat src);
+void thinning(cv::Mat& i);
+
+void pruning(cv::Mat& im);
+Mat dilate(Mat src);
+
+int currentX = 0;
+int currentY = 0;
+
+Mat thing;
+// Trackbar call back function 
+static void onChangeZ(int pos, void* userInput);
+static void onChangeX(int pos, void* userInput);
+static void onChangeY(int pos, void* userInput);
 
 float rotated_angle;
 
@@ -32,31 +44,94 @@ int main()
 	
 	
 	dst = binarization(image);
-	namedWindow("Binarized", WINDOW_NORMAL);
-	//resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
-	resizeWindow("Binarized", 493, 657);
-	imshow("Binarized", image);
+	//namedWindow("Binarized", WINDOW_AUTOSIZE);
+	////resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
+	//resizeWindow("Binarized", 493, 657);
+	//imshow("Binarized", image);
 
 	dst = straightening(dst);
-	namedWindow("Pre-Straightened", WINDOW_NORMAL);
-	//resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
-	resizeWindow("Pre-Straightened", 493, 657);
-	imshow("Pre-Straightened", dst);
+	//namedWindow("Pre-Straightened", WINDOW_NORMAL);
+	////resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
+	//resizeWindow("Pre-Straightened", 493, 657);
+	//imshow("Pre-Straightened", dst);
 
-	Mat thing = rotate(image, rotated_angle);
-	namedWindow("Post-Straightened", WINDOW_NORMAL);
-	//resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
-	resizeWindow("Post-Straightened", 493, 657);
-	imshow("Post-Straightened", thing);
+	thing = rotate(image, rotated_angle);
+	//namedWindow("Post-Straightened", WINDOW_NORMAL);
+	////resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
+	//resizeWindow("Post-Straightened", 493, 657);
+	//imshow("Post-Straightened", thing);
+
+	thinning(thing);
+	namedWindow("Pre Prune", WINDOW_NORMAL);
+	resizeWindow("Pre Prune", thing.cols*3 +200, thing.rows*3 +200);
+	imshow("Pre Prune", thing);
+	pruning(thing);
+	namedWindow("Post Prune", WINDOW_NORMAL);
+	resizeWindow("Post Prune", thing.cols * 3 + 200, thing.rows * 3 + 200);
+	imshow("Post Prune", thing);
 
 
+	//namedWindow("Control Panel", WINDOW_NORMAL);
+	//resizeWindow("Control", 300, 600);
+	//int scale1 = 100;
+	//int scaleX = thing.cols*3;
+	//int scaleY = thing.rows*3;
+	//// create a trackbar 
+	//createTrackbar("Zoom", "Control Panel", &scale1, 1000, onChangeZ);
+	//onChangeZ(0, 0);
+	//createTrackbar("X", "Control Panel", &scaleX, 1000, onChangeX);
+	//onChangeX(0, 0);
+	//createTrackbar("Y", "Control Panel", &scaleY, 1000, onChangeY);
+	//onChangeX(0, 0);
+	//moveWindow("Control Panel", 0, 650);
+	//imshow("Control Panel", 0);
+
+
+	//thing = dilate(thing);
+	//namedWindow("Dilate", WINDOW_AUTOSIZE);
+	////resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
+	//resizeWindow("Dilate", 493, 657);
+	//imshow("Dilate", true);
+
+	////thing = prune(thing);
+	//namedWindow("Prune", WINDOW_AUTOSIZE);
+	////resizeWindow("mywin", 365 * 1.8, 274 * 1.8);
+	//resizeWindow("Dilate", 493, 657);
+	//imshow("Dilate", thing);
 	waitKey(0);
+}
+
+
+// Trackbar call back function 
+static void onChangeZ(int slider, void* userInput) {
+	float scale = float(slider + 1) / 100;
+
+	Mat img_converted;
+	Size s(thing.size().width*scale, (int)(thing.size().height)*scale);
+	resize(thing, img_converted, s);
+	imshow("Scaled", img_converted);
+}
+
+static void onChangeX(int slider, void* userInput) {
+	currentX = -slider;
+	Mat trans_mat = (Mat_<double>(2, 3) << 1, 0, currentX, 0, 1, currentY);
+	Mat img_converted;
+	warpAffine(thing, img_converted, trans_mat, thing.size());
+	imshow("Scaled", img_converted);
+}
+
+static void onChangeY(int slider, void* userInput) {
+	currentY = -slider;
+	Mat trans_mat = (Mat_<double>(2, 3) << 1, 0, currentX, 0, 1, currentY);
+	Mat img_converted;
+	warpAffine(thing, img_converted, trans_mat, thing.size());
+	imshow("Scaled", img_converted);
 }
 
 Mat binarization(Mat img) {
 	float threshVal;
 	threshVal = avgColor(img) * (1 + bK * (sDeviation(img) / bR - 1));
-
+	printf("Thresh: %f", threshVal);
 	threshold(img, dst, threshVal, max_BINARY_value, 1);
 
 	return dst;
@@ -131,10 +206,10 @@ Mat straightening(Mat s_img) {
 
 
 	float blob_angle_deg = rRect.angle;
-	if (rRect.size.width < rRect.size.height) {
+	/*if (rRect.size.width < rRect.size.height) {
 		printf("True \n");
 		blob_angle_deg = 90 + blob_angle_deg;
-	}
+	}*/
 
 
 	//draw
@@ -153,7 +228,7 @@ Mat straightening(Mat s_img) {
 
 Mat rotate(Mat src, float angle) {
 	Mat new_rot_dst;
-	angle = -(90 - angle);
+	//angle = angle ;
 	// code referenced from Lars Schillingmann on 
 	//   https://stackoverflow.com/questions/22041699/rotate-an-image-without-cropping-in-opencv-in-c
 	// get rotation matrix for rotating the image around its center in pixel coordinates
@@ -167,10 +242,138 @@ Mat rotate(Mat src, float angle) {
 
 	cv::warpAffine(src, new_rot_dst, rot, bbox.size());
 
+
+
 	return new_rot_dst;
 
 }
 
-Mat skeleton(Mat src) {
+
+void thinningIteration(cv::Mat& im, int iter)
+{
+	cv::Mat marker = cv::Mat::zeros(im.size(), CV_8UC1);
+
+	for (int i = 1; i < im.rows - 1; i++)
+	{
+		for (int j = 1; j < im.cols - 1; j++)
+		{
+			uchar p2 = im.at<uchar>(i - 1, j);
+			uchar p3 = im.at<uchar>(i - 1, j + 1);
+			uchar p4 = im.at<uchar>(i, j + 1);
+			uchar p5 = im.at<uchar>(i + 1, j + 1);
+			uchar p6 = im.at<uchar>(i + 1, j);
+			uchar p7 = im.at<uchar>(i + 1, j - 1);
+			uchar p8 = im.at<uchar>(i, j - 1);
+			uchar p9 = im.at<uchar>(i - 1, j - 1);
+
+			int A = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1) +
+				(p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1) +
+				(p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1) +
+				(p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
+			int B = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
+			int m1 = iter == 0 ? (p2 * p4 * p6) : (p2 * p4 * p8);
+			int m2 = iter == 0 ? (p4 * p6 * p8) : (p2 * p6 * p8);
+
+			if (A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0)
+				marker.at<uchar>(i, j) = 1;
+		}
+	}
+
+	im &= ~marker;
+}
+
+/**
+* Function for thinning the given binary image
+*
+* @param  im  Binary image with range = 0-255
+// source :https://web.archive.org/web/20160322113207/http://opencv-code.com/quick-tips/implementation-of-thinning-algorithm-in-opencv/
+*/
+void thinning(cv::Mat& im)
+{
+	im /= 255;
+
+	cv::Mat prev = cv::Mat::zeros(im.size(), CV_8UC1);
+	cv::Mat diff;
+
+	do {
+		thinningIteration(im, 0);
+		thinningIteration(im, 1);
+		cv::absdiff(im, prev, diff);
+		im.copyTo(prev);
+	} while (cv::countNonZero(diff) > 0);
+
+	im *= 255;
+}
+
+//void removeSegments(cv::Mat& im) {
+//	Mat labelImage(im.size(), CV_32S);
+//	//int labels = connectedComponentsWithStats(im, CC_STAT_AREA, 8);
+//	//connectedComponentsWithStats();
+//	printf("%d\n", labels);
+//}
+
+void removeJunctions(cv::Mat& im) {
+	std::vector<vector<int>> junctions;
+	for (int i = 1; i < im.rows - 1; i++)
+	{
+		for (int j = 1; j < im.cols - 1; j++)
+		{
+			uchar p2 = im.at<uchar>(i - 1, j);
+			uchar p3 = im.at<uchar>(i - 1, j + 1);
+			uchar p4 = im.at<uchar>(i, j + 1);
+			uchar p5 = im.at<uchar>(i + 1, j + 1);
+			uchar p6 = im.at<uchar>(i + 1, j);
+			uchar p7 = im.at<uchar>(i + 1, j - 1);
+			uchar p8 = im.at<uchar>(i, j - 1);
+			uchar p9 = im.at<uchar>(i - 1, j - 1);
+
+			int totalNeighbors = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
+
+			if (totalNeighbors >= 3) {
+				// mark the point down for future reference (store in array)
+				junctions.push_back({ i,j });
+				// remove point
+				im.at<uchar>(i, j) = 0;
+			}
+		}
+	}
+	for (const std::vector<int> v : junctions) {
+		for (int y : v) {
+			//printf("%d",y);
+		}
+	}
+}
+
+void pruning(cv::Mat& im) {
+	//convert 255 values to 1, 0 values stay at 0
+	im /= 255;
+
+	cv::Mat prev = cv::Mat::zeros(im.size(), CV_8UC1);
+	cv::Mat diff;
+
+	removeJunctions(im);
+	//removeSegments(im);
+
+	// back to 255 to dislay
+	im *= 255;
 
 }
+
+//Mat dilate(Mat src) {
+//	int dilation_type
+//	int dilation_elem = 0;
+//	int dilation_size = 1;
+//	if (dilation_elem == 0) { dilation_type = MORPH_RECT; }
+//	else if (dilation_elem == 1) { dilation_type = MORPH_CROSS; }
+//	else if (dilation_elem == 2) { dilation_type = MORPH_ELLIPSE; }
+//	Mat dilation_dst;
+//
+//	Mat element = getStructuringElement(dilation_type,
+//		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+//		Point(dilation_size, dilation_size));
+//	/// Apply the dilation operation
+//	dilate(src, dilation_dst, element);
+//	dilation_dst = binarization(dilation_dst);
+//	dilation_dst = binarization(dilation_dst);
+//	return dilation_dst;
+//}
